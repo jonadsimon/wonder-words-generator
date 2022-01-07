9d17ab0e646a97ddeffd534ddcec281e297358c1
jonadsimon <jonathan.simon706@gmail.com>
2022-01-06 21:07:29 -0500

    converted strings to word tuples

--- make_puzzle_v2.py
+++ make_puzzle_v2.py
@@ -6,13 +6,13 @@ from collections import Counter
 
 import subprocess
 
-def get_words_for_board(words, board_size, packing_constant=1.1):
+def get_words_for_board(word_tuples, board_size, packing_constant=1.1):
     """Pick a cutoff which is just beyond limit of the board size."""
     # Order the words by length. It's easier to pack shorter words, so prioritize them.
 
     # This is SUPER hacky, should have a Word class that handles these representational differences.
-    words = sorted(words, key=lambda w: len(w.replace(" ", "").replace("-", "")))
-    cum_len = np.cumsum([len(word.replace(" ", "").replace("-", "")) for word in words])
+    word_tuples = sorted(word_tuples, key=lambda wt: len(wt.board))
+    cum_len = np.cumsum([len(wt.board) for wt in word_tuples])
     num_words = None
     for word_idx, cum_letters in enumerate(cum_len):
         # Try to pack in slightly more letters than would fit on the word without overlaps,
@@ -22,7 +22,7 @@ def get_words_for_board(words, board_size, packing_constant=1.1):
             break
     if not num_words:
         raise ValueError(f"Too few semantic neighbor words to pack a {board_size}x{board_size} board.")
-    return words[:num_words]
+    return word_tuples[:num_words]
 
 def make_data_file(board_words, board_size):
     """Generated a temporary data.dzn file to pass to the minizinc script."""
@@ -35,32 +35,34 @@ def make_data_file(board_words, board_size):
         outfile.write(f"words = [| {' | '.join([', '.join(word + 'E'*(len(board_words[-1])-len(word))) for word in board_words])} |];\n")
 
 def make_puzzle(topic, board_size=20):
-    words, hidden_word_dict = get_related_words(topic)
-    words_to_fit = get_words_for_board(words, board_size, 1.10)
-    board_words = [word.replace(" ", "").replace("-", "").upper() for word in words_to_fit]
+    word_tuples, hidden_word_tuple_dict = get_related_words(topic)
+    word_tuples_to_fit = get_words_for_board(word_tuples, board_size, 1.0)
+    # words, hidden_word_dict = get_related_words(topic)
+    # words_to_fit = get_words_for_board(words, board_size, 1.10)
+    # board_words = [word.replace(" ", "").replace("-", "").upper() for word in words_to_fit]
 
-    print(words_to_fit)
-    print(hidden_word_dict)
+    print("\n", [wt.pretty for wt in word_tuples_to_fit], "\n")
+    print({l: wt.pretty for l,wt in hidden_word_tuple_dict.items()}, "\n")
 
     # Generate Minizinc data file to feed into the parameterizd script.
-    make_data_file(board_words, board_size)
+    make_data_file([wt.board for wt in word_tuples_to_fit], board_size)
 
     # Run the script
     raw_board = subprocess.Popen("/Applications/MiniZincIDE.app/Contents/Resources/minizinc --solver Chuffed MiniZinc_scripts/parameterized_board_generator.mzn tmp/data.dzn", shell=True, stdout=subprocess.PIPE).stdout.read()
 
     board = [line.strip().split() for line in raw_board.decode("utf-8").strip().split("\n")]
     blank_locs = [(i,j) for i,row in enumerate(board) for j,letter in enumerate(row) if letter == "_"]
-    if len(blank_locs) not in hidden_word_dict:
+    if len(blank_locs) not in hidden_word_tuple_dict:
         print(len(blank_locs), blank_locs)
-        print(hidden_word_dict)
+        print(hidden_word_tuple_dict)
         raise ValueError("Number of remaining blanks does not fit any available word.")
     k = 0
     for i, j in blank_locs:
-        board[i][j] = hidden_word_dict[len(blank_locs)][k]
+        board[i][j] = hidden_word_tuple_dict[len(blank_locs)].board[k]
         k += 1
     for row in board:
         print(" ".join(row))
-    print("\n", "   ".join(sorted(words_to_fit)))
+    print("\n", "   ".join(sorted([wt.pretty for wt in word_tuples_to_fit])))
 
 if __name__ == "__main__":
-    make_puzzle("flamboyant", 15)
+    make_puzzle("dreaming", 15)
--- semantic_neighbors.py
+++ semantic_neighbors.py
@@ -8,6 +8,89 @@ import math
 from Levenshtein import distance as levenshtein_distance
 import unidecode
 from nltk.stem.porter import PorterStemmer
+from collections import namedtuple
+
+WordTuple = namedtuple('WordTuple', ['pretty', 'board'])
+
+# Consider adding an additional Word wrapper to handle downstream transformations
+# Might be easier to hold on on this until *after* filtering has been performed
+# Then words will be immutable and can be treated as named_tuples
+
+# Rephrase 3 univariate filters and 4 bivariate filters to fit with these new functions
+# return and print the filtered words for each
+
+def univariate_filtering(words, filter_func):
+    """Identify words in the list meeting the filtering condition, and remove them"""
+    pass
+
+def bivariate_filtering(words, similarity_func):
+    """Identify pairs of words in the list meeting the similarity condition, and remove the longer of the two"""
+    pass
+
+def filter_word_strings(words):
+    # Remove words shorter than 4 letters.
+    words = [word for word in words if len(word) > 3]
+
+    # Strip accents from letters
+    words = [unidecode.unidecode(word) for word in words]
+
+    # Remove words which contain characters other than: a-z/A-Z, spaces, hyphens.
+    words = [word for word in words if not re.search(r"[^a-zA-Z\-\s]", word)]
+
+    # Remove words that are superstrings of another existing word.
+    super_words = []
+    for word_sub in words:
+        for word_super in words:
+            if word_sub in word_super and word_sub != word_super:
+                super_words.append(word_super)
+    words = [word for word in words if word not in super_words]
+    print(f"\nRemoved too-similar words (superstring): {', '.join(list(super_words))}")
+
+    # Remove words of length ≥6 for which the first 70% of letters are the same
+    # (Cutoffs chosen manually to increase word diversity, should make more flexible.)
+    # ≥5/6 letters, ≥5/7 letters, ≥6/8 letters, etc
+    # By default pick the latter word as the one to discard
+    too_similar_words = set()
+    for i in range(len(words)):
+        for j in range(i+1, len(words)):
+            if min(len(words[i]), len(words[j])) >= 6:
+                prefix_len = math.ceil((0.7*min(len(words[i]), len(words[j]))))
+                if words[i][:prefix_len+1] == words[j][:prefix_len+1]:
+                    too_similar_words.add(words[j])
+    words = [word for word in words if word not in too_similar_words]
+    if too_similar_words:
+        print(f"\nRemoved too-similar words (long-prefix): {', '.join(list(too_similar_words))}")
+
+    # If a given word is dist ≤ 1 from another, need to remove one of the two
+    # Should err on the side of remove the word which occurred later in the list
+    # Therefore build up iteration so word2_idx > word1_idx
+    too_similar_words = set()
+    for i in range(len(words)):
+        for j in range(i+1, len(words)):
+            # If the two words differ only by a single letter
+            # Deals with alternate spellings, e.g. "calender" vs "calendar"
+            # Add a min-length constraint to avoid misfiring on e.g. tide/time, suda/susa
+            if levenshtein_distance(words[i], words[j]) <= 1 and min(len(words[i]), len(words[j])) >= 5:
+                too_similar_words.add(words[j])
+    words = [word for word in words if word not in too_similar_words]
+    if too_similar_words:
+        print(f"\nRemoved too-similar words (Levenshtein): {', '.join(list(too_similar_words))}")
+
+    # Substrings + Levenshtein don't catch cases where both strings are short, e.g. time/timing, marked/marking
+    # Solution is to apply a stemmer and delete matching words
+    too_similar_words = set()
+    porter_stemmer = PorterStemmer()
+    for i in range(len(words)):
+        for j in range(i+1, len(words)):
+            if porter_stemmer.stem(words[i]) == porter_stemmer.stem(words[j]):
+                too_similar_words.add(words[j])
+    words = [word for word in words if word not in too_similar_words]
+    if too_similar_words:
+        print(f"\nRemoved too-similar words (stemming): {', '.join(list(too_similar_words))}")
+
+    return words
+
+
 
 def get_related_words(word, score_cutoff=0.3, neighbors_cutoff=150):
     """Fetch related words from relatedwords.org, and clean them up.
@@ -36,74 +119,18 @@ def get_related_words(word, score_cutoff=0.3, neighbors_cutoff=150):
          else:
              break
 
-    # Remove words shorter than 4 letters.
-    words = [word for word in words if len(word) > 3]
+    words = filter_word_strings(words)
 
-    # Strip accents from letters
-    words = [unidecode.unidecode(word) for word in words]
-
-    # Remove words which contain characters other than: a-z/A-Z, spaces, hyphens.
-    words = [word for word in words if not re.search(r"[^a-zA-Z\-\s]", word)]
-
-    # Remove words that are superstrings of another existing word.
-    super_words = []
-    for word_sub in words:
-        for word_super in words:
-            if word_sub in word_super and word_sub != word_super:
-                super_words.append(word_super)
-    words = [word for word in words if word not in super_words]
-    print(f"Removed too-similar words (superstring): {', '.join(list(super_words))}")
-
-    # Remove words of length ≥6 for which the first 70% of letters are the same
-    # (Cutoffs chosen manually to increase word diversity, should make more flexible.)
-    # ≥5/6 letters, ≥5/7 letters, ≥6/8 letters, etc
-    # By default pick the latter word as the one to discard
-    too_similar_words = set()
-    for i in range(len(words)):
-        for j in range(i+1, len(words)):
-            if min(len(words[i]), len(words[j])) >= 6:
-                prefix_len = math.ceil((0.7*min(len(words[i]), len(words[j]))))
-                if words[i][:prefix_len+1] == words[j][:prefix_len+1]:
-                    too_similar_words.add(words[j])
-    words = [word for word in words if word not in too_similar_words]
-    if too_similar_words:
-        print(f"Removed too-similar words (long-prefix): {', '.join(list(too_similar_words))}")
-
-    # If a given word is dist ≤ 1 from another, need to remove one of the two
-    # Should err on the side of remove the word which occurred later in the list
-    # Therefore build up iteration so word2_idx > word1_idx
-    too_similar_words = set()
-    for i in range(len(words)):
-        for j in range(i+1, len(words)):
-            # If the two words differ only by a single letter
-            # Deals with alternate spellings, e.g. "calender" vs "calendar"
-            # Add a min-length constraint to avoid misfiring on e.g. tide/time, suda/susa
-            if levenshtein_distance(words[i], words[j]) <= 1 and min(len(words[i]), len(words[j])) >= 5:
-                too_similar_words.add(words[j])
-    words = [word for word in words if word not in too_similar_words]
-    if too_similar_words:
-        print(f"Removed too-similar words (Levenshtein): {', '.join(list(too_similar_words))}")
-
-    # Substrings + Levenshtein don't catch cases where both strings are short, e.g. time/timing, marked/marking
-    # Solution is to apply a stemmer and delete matching words
-    too_similar_words = set()
-    porter_stemmer = PorterStemmer()
-    for i in range(len(words)):
-        for j in range(i+1, len(words)):
-            if porter_stemmer.stem(words[i]) == porter_stemmer.stem(words[j]):
-                too_similar_words.add(words[j])
-    words = [word for word in words if word not in too_similar_words]
-    if too_similar_words:
-        print(f"Removed too-similar words (stemming): {', '.join(list(too_similar_words))}")
-
+    # Convert words to word-tuples, and operate on these going forward
+    word_tuples = [WordTuple(pretty=word, board=word.replace(" ", "").replace("-", "").upper()) for word in words]
 
     # Identify words of varying length to hide in the puzzle, and remove them from the set of words being placed.
-    hidden_words = {}
-    for word in words:
-        # This is hacky, should have a word class that handles these representational differences.
-        letters = word.replace(" ", "").replace("-", "").upper()
-        if len(letters) not in hidden_words:
-            hidden_words.update({len(letters): letters})
-    words = [word for word in words if word.replace(" ", "").replace("-", "").upper() not in hidden_words.values()]
+    hidden_word_tuple_dict = {}
+    for word_tuple in word_tuples:
+        if len(word_tuple.board) not in hidden_word_tuple_dict:
+            hidden_word_tuple_dict.update({len(word_tuple.board): word_tuple})
+    word_tuples = [word_tuple for word_tuple in word_tuples if word_tuple not in hidden_word_tuple_dict.values()]
 
-    return words, hidden_words
+    return word_tuples, hidden_word_tuple_dict
+
+    # ok, still ugly, but at least all the data is there
